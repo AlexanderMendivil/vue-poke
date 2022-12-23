4d3be32dc6cdda8493f630efe895bd8053ba3982
Alexander Mendivil <alexanderima@outlook.es>
2022-12-23 16:18:46 -0700

    app completada

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,16 +1,30 @@
+import pokemonApi from "@/api/pokemon.api"
+
 const getPokemons = () =>{
     const pokemonsArr = Array.from( Array(650) )
     return pokemonsArr.map(( _, index )=> index + 1 )
 }
 
-const getPokemonOptions = () =>{
+const getPokemonOptions = async () =>{
     const mixedPokemons = getPokemons().sort(() => Math.random() -0.5 )
-    getPokemonsNames(mixedPokemons.splice(0, 4))
-    console.log(mixedPokemons)
+    return await getPokemonsNames(mixedPokemons.splice(0, 4))
 }
 
-const getPokemonsNames = ([ a, b, c, d ] = [] ) => {
+const getPokemonsNames = async ([ a, b, c, d ] = [] ) => {
+    const promiseArr = [
+        pokemonApi.get(`/${a}`),
+        pokemonApi.get(`/${b}`),
+        pokemonApi.get(`/${c}`),
+        pokemonApi.get(`/${d}`),
+    ]
 
+    const [ p1, p2, p3, p4 ] = await Promise.all( promiseArr )
 
+    return [ 
+        {name: p1.data.name, id: p1.data.id},
+        {name: p2.data.name, id: p2.data.id},
+        {name: p3.data.name, id: p3.data.id},
+        {name: p4.data.name, id: p4.data.id},
+    ]
 }
 export default getPokemonOptions;
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,8 +1,17 @@
 <template>
-    <h1>¿Quien es este pokemon?</h1>
-      
-      <PokemonPicture :pokemonId="4" :showPokemon="true"/>
-      <PokemonOption/>
+
+    <div v-if="pokemon">
+        <h1>¿Quien es este pokemon?</h1>
+        <PokemonPicture :pokemonId="pokemon.id" :showPokemon="showPokemon"/>
+        <PokemonOption :pokemons="pokemonArr" @selection="checkAnswer"/>
+
+        <template v-if="showAnswer">
+            <h2 class="fade-in">{{ message }}</h2>
+            <button @click="newGame">Nuevo juego</button>
+        </template>
+    </div>
+
+    <h1 v-else>Espere por favor...</h1>
 
 </template>
 
@@ -15,6 +24,42 @@ export default {
     components:{
         PokemonOption,
         PokemonPicture
-    }   
+    },
+    data(){
+        return{
+            pokemonArr: [],
+            pokemon: null,
+            showPokemon: false,
+            showAnswer: false,
+            message: '',
+        }
+    },
+    methods:{
+        async mixPokemonArray(){
+            this.pokemonArr = await getPokemonOptions()
+            const rndInt = Math.floor( Math.random() * 4 )
+            this.pokemon = this.pokemonArr[ rndInt ]
+        },
+        checkAnswer(pokemonId){
+            this.showAnswer = true
+            this.showPokemon = true 
+            if(pokemonId === this.pokemon.id){
+                this.message = `Correcto, ${this.pokemon.name}`
+            }else{
+                this.message = `Oops era: ${this.pokemon.name}`
+
+            }
+        },
+        newGame(){
+            this.pokemonArr = []
+            this.showAnswer = false
+            this.showPokemon = false 
+            this.pokemon = null
+            this.mixPokemonArray()
+        }
+    },
+        mounted(){
+            this.mixPokemonArray()
+        }
 }
 </script>
